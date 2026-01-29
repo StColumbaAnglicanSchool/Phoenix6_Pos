9b399a1c50e2a2487baa775275df3d1dcebb07b3
Yr7 FLL Coding Laptop <43365474+w-reynolds@users.noreply.github.com>
2026-01-29 15:40:15 +1100

    Motion Magic added to Turret motor

--- src/main/cpp/Robot.cpp
+++ src/main/cpp/Robot.cpp
@@ -3,15 +3,77 @@
 // the WPILib BSD license file in the root directory of this project.
 
 #include "Robot.h"
+#include <iostream>
+#include "ctre/phoenix6/SignalLogger.hpp"
 
-Robot::Robot() {}
-void Robot::RobotPeriodic() {}
+using namespace ctre::phoenix6;
+
+Robot::Robot() {
+
+  ctre::phoenix6::SignalLogger::EnableAutoLogging(false);
+  
+  configs::TalonFXConfiguration configs{};
+
+  configs::FeedbackConfigs &fdb = configs.Feedback;
+  fdb.SensorToMechanismRatio = 9;
+
+  configs::MotionMagicConfigs &mm = configs.MotionMagic;
+  mm.MotionMagicCruiseVelocity = 10_tps;
+  mm.MotionMagicAcceleration = 10_tr_per_s_sq;
+
+  configs.Slot0.kS = 0.25;
+  configs.Slot0.kV = 0.12;
+  configs.Slot0.kA = 0.01;
+  configs.Slot0.kP = 80.0;
+  configs.Slot0.kI = 0;
+  configs.Slot0.kD = 0.1;
+
+  configs.Voltage.PeakForwardVoltage = 12_V;
+  configs.Voltage.PeakReverseVoltage = -12_V;
+
+  ctre::phoenix::StatusCode status = ctre::phoenix::StatusCode::StatusCodeNotInitialized;
+  for (int i = 0; i < 5; ++i) {
+    status = m_Turret.GetConfigurator().Apply(configs);
+    if (status.IsOK()) break;
+  }
+  if (!status.IsOK()) {
+    std::cout << "Could not apply configs, error code: " << status.GetName() << std::endl;
+  }
+
+  m_Turret.SetPosition(0_tr);
+}
+
+void Robot::RobotPeriodic() {
+  if (++m_printCount >= 10) {
+    m_printCount = 0;
+    //std::cout << "Pos: " << m_Turret.GetPosition() << std::endl;
+    //std::cout << "Vel: " << m_Turret.GetVelocity() << std::endl;
+    //std::cout << std::endl;
+  }
+}
 
 void Robot::AutonomousInit() {}
 void Robot::AutonomousPeriodic() {}
 
 void Robot::TeleopInit() {}
-void Robot::TeleopPeriodic() {}
+void Robot::TeleopPeriodic() {
+//   double Y = m_DriverController.GetY();
+//   if (fabs(Y) < 0.1) Y = 0;
+
+//   m_Turret.SetControl(m_mmReq.WithPosition(Y * 10_tr).WithSlot(0));
+
+  if (m_DriverController.GetRawButton(7)) {
+    m_Turret.SetControl(m_mmReq.WithPosition(360_deg).WithSlot(0));
+  }
+
+  if (m_DriverController.GetRawButton(8)) {
+    m_Turret.SetControl(m_mmReq.WithPosition(0_tr).WithSlot(0));
+  }
+
+  if (m_DriverController.GetRawButton(9)) {
+    m_Turret.SetControl(m_mmReq.WithPosition(-360_deg).WithSlot(0));
+  }
+}
 
 void Robot::DisabledInit() {}
 void Robot::DisabledPeriodic() {}
--- src/main/include/Robot.h
+++ src/main/include/Robot.h
@@ -9,7 +9,10 @@
 #include <frc/TimedRobot.h>
 #include "ctre/phoenix6/TalonFX.hpp"
 #include <frc/smartdashboard/SendableChooser.h>
-#include <frc/XboxController.h>
+// #include <frc/XboxController.h>
+#include <frc/Joystick.h>
+
+#define TURRET
 
 class Robot : public frc::TimedRobot {
  public:
@@ -30,4 +33,22 @@ class Robot : public frc::TimedRobot {
 
   void SimulationInit() override;
   void SimulationPeriodic() override;
+
+ private:
+  frc::SendableChooser<std::string> m_chooser;
+  const std::string kAutoNameDefault = "Default";
+  const std::string kAutoNameCustom = "My Auto";
+  std::string m_autoSelected;
+
+  ctre::phoenix6::CANBus kCANBus{"rio"};
+  ctre::phoenix6::controls::MotionMagicVoltage m_mmReq{0_tr};
+  int m_printCount = 0;
+
+  #ifdef TURRET
+  ctre::phoenix6::hardware::TalonFX m_Turret{2, kCANBus};
+  #endif
+
+  ctre::phoenix6::controls::PositionVoltage m_turretVoltage = ctre::phoenix6::controls::PositionVoltage{0_tr}.WithSlot(0);
+  ctre::phoenix6::controls::StaticBrake m_brake{};
+  frc::Joystick m_DriverController{0};
 };
