60401d38096804dc99508f1b73d898268d18cf9e
NoahW <Noah-Williams@scas.nsw.edu.au>
2026-02-01 20:36:10 +1100

    edited some motor configs, will need revision in Shooter.cpp

--- src/main/cpp/subsystems/Shooter.cpp
+++ src/main/cpp/subsystems/Shooter.cpp
@@ -27,26 +27,69 @@ ShooterSubsystem::ShooterSubsystem() {
   configs.Voltage.PeakForwardVoltage = 12_V;
   configs.Voltage.PeakReverseVoltage = -12_V;
 
+  /* Voltage-based velocity requires a feed forward to account for the back-emf of the motor */
+  configs.Slot1.kS = 0.1; // To account for friction, add 0.1 V of static feedforward
+  configs.Slot1.kV = 0.116; // Kraken X60 is a 500 kV motor, 500 rpm per V = 8.333 rps per V, 1/8.33 = 0.12 volts / rotation per second
+
   ctre::phoenix::StatusCode status =
       ctre::phoenix::StatusCode::StatusCodeNotInitialized;
+  #ifdef TURRET
   for (int i = 0; i < 5; ++i) {
-    status = m_TurretMotor.GetConfigurator().Apply(configs);
+    status = m_LTurretMotor.GetConfigurator().Apply(configs.Slot0);
     if (status.IsOK())
       break;
   }
   if (!status.IsOK()) {
-    std::cout << "Could not apply configs, error code: " << status.GetName()
-              << std::endl;
+    std::cout << "Could not apply configs, error code: " << status.GetName() << std::endl;
   }
+  for (int i = 0; i < 5; ++i) {
+    status = m_RTurretMotor.GetConfigurator().Apply(configs.Slot0);
+    if (status.IsOK())
+      break;
+  }
+  if (!status.IsOK()) {
+    std::cout << "Could not apply configs, error code: " << status.GetName() << std::endl;
+  }
+  #endif
+  
+  #ifdef SHOOTER
+  for (int i = 0; i < 5; ++i) {
+    status = m_LShooterMotor.GetConfigurator().Apply(configs.Slot1);
+    if (status.IsOK())
+      break;
+  }
+  // configs::TalonFXConfiguration LShooterInverted;
+  // LShooterInverted.motorOutput.Inverted = configs.motorOutput.Inverted.Clockwise_Positive;
+  // status = m_LShooterMotor.GetConfigurator().Apply(LShooterInverted);
+  if (!status.IsOK()) {
+    std::cout << "Could not apply configs, error code: " << status.GetName() << std::endl;
+  }
+  for (int i = 0; i < 5; ++i) {
+    status = m_RShooterMotor.GetConfigurator().Apply(configs.Slot1);
+    if (status.IsOK())
+      break;
+  }
+  if (!status.IsOK()) {
+    std::cout << "Could not apply configs, error code: " << status.GetName() << std::endl;
+  }
+  #endif
 };
 
 // This method will be called once per scheduler run
 void ShooterSubsystem::Periodic() {}
 
 void ShooterSubsystem::SetTurretAngle(units::angle::degree_t Angle) {
-  m_TurretMotor.SetControl(m_MotionMagicRequest.WithPosition(Angle).WithSlot(0));
+  m_LTurretMotor.SetControl(m_MotionMagicRequest.WithPosition(Angle).WithSlot(0));
 };
 
 void ShooterSubsystem::SetShooterActive(bool Trigger) {
-
+  #ifdef SHOOTER
+  if (Trigger == true) {
+    m_LShooterMotor.SetControl(m_ShooterVOut.WithVelocity(50*1_tps).withSlot(1));
+    m_RShooterMotor.SetControl(m_ShooterVOut.WithVelocity(50*1_tps).withSlot(1));
+  } else {
+    m_LShooterMotor.SetControl(0.0_V).withSlot(1);
+    m_RShooterMotor.SetControl(0.0_V).withSlot(1);
+  } 
+  #endif
 };
--- src/main/include/subsystems/Shooter.h
+++ src/main/include/subsystems/Shooter.h
@@ -7,6 +7,11 @@
 #include <frc2/command/SubsystemBase.h>
 #include <ctre/phoenix6/TalonFX.hpp>
 
+// #define SHOOTER
+// #define FEEDER
+// #define HOPPER
+#define TURRET
+
 class ShooterSubsystem : public frc2::SubsystemBase {
  public:
   ShooterSubsystem();
@@ -25,5 +30,33 @@ class ShooterSubsystem : public frc2::SubsystemBase {
   // declared private and exposed only through public methods.
   ctre::phoenix6::CANBus kCANBus{"rio"};
   ctre::phoenix6::controls::MotionMagicVoltage m_MotionMagicRequest{0_tr};
-  ctre::phoenix6::hardware::TalonFX m_TurretMotor{2, kCANBus};
+
+  ctre::phoenix6::controls::VoltageOut m_FeederVOut =
+      ctre::phoenix6::controls::VoltageOut{0.0_V};
+  ctre::phoenix6::controls::VelocityVoltage m_ShooterVOut =
+      ctre::phoenix6::controls::VelocityVoltage{0_tps}.WithSlot(1);
+  ctre::phoenix6::controls::VoltageOut m_HopperVOut =
+      ctre::phoenix6::controls::VoltageOut{0.0_V};
+  ctre::phoenix6::controls::VoltageOut m_IntakeVOut =
+      ctre::phoenix6::controls::VoltageOut{0.0_V};
+  /* Be able to switch which control request to use based on a button press */
+  /* Start at velocity 0, use slot 1 */
+  ctre::phoenix6::controls::VelocityVoltage m_velocityVoltage =
+      ctre::phoenix6::controls::VelocityVoltage{0_tps}.WithSlot(1);
+
+#ifdef SHOOTER
+  ctre::phoenix6::hardware::TalonFX m_LShooterMotor{4, kCANBus};
+  ctre::phoenix6::hardware::TalonFX m_RShooterMotor{5, kCANBus};
+#endif
+#ifdef FEEDER
+  ctre::phoenix6::hardware::TalonFX m_LFeederMotor{1, kCANBus};
+  ctre::phoenix6::hardware::TalonFX m_RFeederMotor{7, kCANBus};
+#endif
+#ifdef HOPPER
+  ctre::phoenix6::hardware::TalonFX m_LHopperMotor{3, kCANBus};
+#endif
+#ifdef TURRET
+  ctre::phoenix6::hardware::TalonFX m_LTurretMotor{2, kCANBus};
+  ctre::phoenix6::hardware::TalonFX m_RTurretMotor{6, kCANBus};
+#endif
 };
