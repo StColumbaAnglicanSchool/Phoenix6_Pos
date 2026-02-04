a558b227b0e0c9655402e3144ecfcd075f1b0047
Warren Reynolds <43365474+w-reynolds@users.noreply.github.com>
2026-02-04 23:09:14 +1100

    Turret Code Cleanup/ Add Comments and TODO

--- src/main/cpp/subsystems/Turret.cpp
+++ src/main/cpp/subsystems/Turret.cpp
@@ -15,16 +15,19 @@ Turret::Turret(int shooterMotorCANBusID, int feederMotorCANBusID,
       m_turretMotor(turretMotorCANBusID),
       m_turretCANCoder(turretCANCoderID),
       m_turretName(turretName) {
+  // Create 3 config objects to apply to the motors and CANCoder
   ctre::phoenix6::configs::TalonFXConfiguration turretConfig{};
   ctre::phoenix6::configs::TalonFXConfiguration shooterConfig{};
   ctre::phoenix6::configs::TalonFXConfiguration feederConfig{};
 
-  configs::FeedbackConfigs& Feedback = turretConfig.Feedback;
-  Feedback.SensorToMechanismRatio = 20;
+  // Populate the turretConfig object
+  turretConfig.MotorOutput.Inverted =
+      signals::InvertedValue::Clockwise_Positive;
 
-  configs::MotionMagicConfigs& MotionMagic = turretConfig.MotionMagic;
-  MotionMagic.MotionMagicCruiseVelocity = 0.3_tps;
-  MotionMagic.MotionMagicAcceleration = 10_tr_per_s_sq;
+  turretConfig.Feedback.SensorToMechanismRatio = 20;
+
+  turretConfig.MotionMagic.MotionMagicCruiseVelocity = 0.3_tps;
+  turretConfig.MotionMagic.MotionMagicAcceleration = 10_tr_per_s_sq;
 
   turretConfig.Slot0.kS = 0.1;
   turretConfig.Slot0.kV = 0.12;
@@ -36,9 +39,26 @@ Turret::Turret(int shooterMotorCANBusID, int feederMotorCANBusID,
   turretConfig.Voltage.PeakForwardVoltage = 8_V;
   turretConfig.Voltage.PeakReverseVoltage = -8_V;
 
-  turretConfig.MotorOutput.Inverted =
-      signals::InvertedValue::Clockwise_Positive;
+  // Create a StatusCode Object so we can collect Error information
+  // if applying the config object fails
+  ctre::phoenix::StatusCode status =
+      ctre::phoenix::StatusCode::StatusCodeNotInitialized;
 
+  // Try to apply the turret motor config object up to 5 times.
+  for (int i = 0; i < 5; ++i) {
+    status = m_turretMotor.GetConfigurator().Apply(turretConfig);
+    if (status.IsOK())
+      break;
+  }
+  if (!status.IsOK()) {
+    std::cout << "Could not apply turret config, error code: "
+              << status.GetName() << std::endl;
+  }
+
+  // Populate the Shooter Config Object
+  //-----------TODO---------- Test and set inverted value for the shooter
+  // shooterConfig.MotorOutput.Inverted =
+  //    signals::InvertedValue::Clockwise_Positive;
 
   shooterConfig.Slot0.kS = 0.1;
   shooterConfig.Slot0.kV = 0.12;
@@ -50,20 +70,7 @@ Turret::Turret(int shooterMotorCANBusID, int feederMotorCANBusID,
   shooterConfig.Voltage.PeakForwardVoltage = 12_V;
   shooterConfig.Voltage.PeakReverseVoltage = -12_V;
 
-  
-  ctre::phoenix::StatusCode status =
-      ctre::phoenix::StatusCode::StatusCodeNotInitialized;
-
-  for (int i = 0; i < 5; ++i) {
-    status = m_turretMotor.GetConfigurator().Apply(turretConfig);
-    if (status.IsOK())
-      break;
-  }
-  if (!status.IsOK()) {
-    std::cout << "Could not apply turret config, error code: "
-              << status.GetName() << std::endl;
-  }
-
+  // Try to apply the Shooter motor config object up to 5 times.
   for (int i = 0; i < 5; ++i) {
     status = m_shooterMotor.GetConfigurator().Apply(shooterConfig);
     if (status.IsOK())
@@ -71,19 +78,26 @@ Turret::Turret(int shooterMotorCANBusID, int feederMotorCANBusID,
   }
   if (!status.IsOK()) {
     std::cout << "Could not apply configs, error code: " << status.GetName()
-    << std::endl;
+              << std::endl;
   }
 };
 
-void Turret::InitializeTurretAngle() {}
+void Turret::InitializeTurretAngle() {
+  // --------------------------TODO--------------------------
+  // Add code here to read the CANCoder
+  // position and apply it to the Turret Motor
+}
 
 void Turret::SetTurretAngle(units::angle::degree_t Angle) {
   fmt::println("[METHOD] Moving to {} Degrees...", Angle);
-  m_turretMotor.SetControl(m_motionMagicControl.WithPosition(Angle).WithSlot(0));
+  m_turretMotor.SetControl(
+      m_motionMagicControl.WithPosition(Angle).WithSlot(0));
 };
 
 units::angle::degree_t Turret::GetTurretAngle() {
-
+  // --------------------TODO-----------------------
+  // Remove return 0_deg and add code here to return the current angle of the turret
+  return 0_deg;
 };
 
 void Turret::SetShooterSpeed(units::turns_per_second_t speed) {
@@ -97,5 +111,7 @@ void Turret::SetShooterSpeed(units::turns_per_second_t speed) {
 };
 
 bool Turret::IsShooterAtSpeed() {
-
+  // ------------------TODO----------------------
+  // Remove return false add code here to check if the turret has reached the set speed
+  return false;
 };
--- src/main/include/subsystems/Turret.h
+++ src/main/include/subsystems/Turret.h
@@ -6,7 +6,6 @@
 
 #include <numbers>
 
-//#include <frc2/command/SubsystemBase.h>
 #include <ctre/phoenix6/TalonFX.hpp>
 #include <ctre/phoenix6/controls/MotionMagicVoltage.hpp>
 #include <ctre/phoenix6/controls/VelocityVoltage.hpp>
@@ -21,17 +20,39 @@ using namespace ctre::phoenix6;
 class Turret {
  public:
   Turret(int shooterMotorCANBusID, int feederMotorCANBusID,
-                   int turretMotorCANBusID, int turretCANCoderID,
-                   std::string turretName);
-
+         int turretMotorCANBusID, int turretCANCoderID, std::string turretName);
+  /**
+   * Initialize the Turret Motor internal position register
+   * using the absolute position of the turret CANCoder.
+   */
   void InitializeTurretAngle();
 
+  /**
+   * Motion Magic the turret to the Angle.
+   *
+   * @param Angle The Angle to move to in Degrees
+   */
   void SetTurretAngle(units::angle::degree_t Angle);
 
+  /**
+   * Read the turret angle based on the turret motor internal position
+   *
+   * @return Curret turret angle in degrees
+   */
   units::angle::degree_t GetTurretAngle();
 
+  /**
+   * Set the speed of the Shooter
+   * 
+   * @param speed in turns_per_second
+   */
   void SetShooterSpeed(units::turns_per_second_t speed);
 
+  /**
+   * Checks if the shooter has reached the set speed
+   * 
+   * @return returns True or False
+   */
   bool IsShooterAtSpeed();
 
  private:
@@ -42,19 +63,22 @@ class Turret {
   hardware::TalonFX m_turretMotor;
   hardware::CANcoder m_turretCANCoder;
   std::string m_turretName;
-  
-  units::turns_per_second_t m_targetVelocity{0_tps};
-  units::degree_t m_targetTurretAngle{0_deg};  
 
+  // Members variables to hold the most recent setpoints
+  units::turns_per_second_t m_targetVelocity{0_tps};
+  units::degree_t m_targetTurretAngle{0_deg};
+
+  // Create control objects to use the variuos modes of control
   controls::VelocityVoltage m_voltageVelocity{0_tps};
   controls::VoltageOut m_voltageOut{0.0_V};
   controls::PositionVoltage m_positionControl{0_tr};
   controls::MotionMagicVoltage m_motionMagicControl{0_tr};
 
+  // Example Alert messeage that can be set in code
   frc::Alert m_shooterAlertInfo{"Shooter Subsystem Info",
                                 frc::Alert::AlertType::kInfo};
   frc::Alert m_shooterAlertWarning{"Shooter Subsystem Warning",
                                    frc::Alert::AlertType::kWarning};
   frc::Alert m_shooterAlertError{"Shooter Subsystem Error",
                                  frc::Alert::AlertType::kError};
-};
+};  // End of Turret Class Definition
